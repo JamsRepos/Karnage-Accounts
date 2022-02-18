36029ba5b87c96b060c91a1f5bb09c0be9fc8417
Venom <runningnuke32@gmail.com>
2022-02-18 06:54:16 +0000

    base files

---
+++ app/config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+JELLYFIN_API_KEY = (os.getenv("JELLYFIN_API_KEY"))
+JFA_GO_API_KEY = (os.getenv("JFA_GO_API_KEY"))
+OMBI_API_KEY = (os.getenv("OMBI_API_KEY"))
