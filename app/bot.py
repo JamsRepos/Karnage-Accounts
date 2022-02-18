a4734895eb445f40ff3c277059e8d896576a07ca
Venom <runningnuke32@gmail.com>
2022-02-18 06:57:51 +0000

    black formating

--- app/bot.py
+++ app/bot.py
@@ -2,7 +2,7 @@ import os
 import disnake
 from disnake.ext import commands
 
-from config import BOT_TOKEN,OMBI_API_KEY,JELLYFIN_API_KEY,JFA_GO_API_KEY
+from config import BOT_TOKEN, OMBI_API_KEY, JELLYFIN_API_KEY, JFA_GO_API_KEY
 
 bot = commands.Bot(command_prefix=">", test_guilds=[676592448620724254])
 
@@ -10,10 +10,15 @@ for cogs in os.listdir("./app/cogs"):
     if cogs.endswith(".py"):
         bot.load_extension(f"cogs.{cogs[:-3]}")
 
+
 @bot.event
 async def on_ready():
-    print(f'Bot User: {bot.user}')
-    print(f'Bot UserID: {bot.user.id}')
-    await bot.change_presence(status=disnake.Status, activity=disnake.Activity(type=disnake.ActivityType.watching, name="Karna.ge") )
+    print(f"Bot User: {bot.user}")
+    print(f"Bot UserID: {bot.user.id}")
+    await bot.change_presence(
+        status=disnake.Status,
+        activity=disnake.Activity(type=disnake.ActivityType.watching, name="Karna.ge"),
+    )
+
 
 bot.run(BOT_TOKEN)
--- app/config.py
+++ app/config.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-JELLYFIN_API_KEY = (os.getenv("JELLYFIN_API_KEY"))
-JFA_GO_API_KEY = (os.getenv("JFA_GO_API_KEY"))
-OMBI_API_KEY = (os.getenv("OMBI_API_KEY"))
+JELLYFIN_API_KEY = os.getenv("JELLYFIN_API_KEY")
+JFA_GO_API_KEY = os.getenv("JFA_GO_API_KEY")
+OMBI_API_KEY = os.getenv("OMBI_API_KEY")
