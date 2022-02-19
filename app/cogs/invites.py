0ca3695d1915c462530843f4f30ec9f4a985214c
James Finch <LubricantJam@users.noreply.github.com>
2022-02-19 01:12:15 +0000

    Fixed error with indentation.

--- app/cogs/invites.py
+++ app/cogs/invites.py
@@ -54,14 +54,13 @@ class Invites(commands.Cog):
             oldRole = next(role for role in before.roles if role not in after.roles)
 
             if str(oldRole.id) in MEMBERSHIP_ROLES:
+                print('role removed')
                 # TODO: Add the logic for when the role is removed such as removing role_id, purchase_date, invite_reset, invites_used.
 
 
 
     @commands.slash_command(description="Sends an invite to the target User.")
-    async def invite(
-        self, inter: disnake.ApplicationCommandInteraction, user: disnake.User
-    ):
+    async def invite(self, inter: disnake.ApplicationCommandInteraction, user: disnake.User):
         """Creates an invite"""
 
         callerName = str(inter.user)
