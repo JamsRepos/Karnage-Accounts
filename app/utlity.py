ef60bffcf0232332d1a4adfb827389733c5ad5ea
Venom <runningnuke32@gmail.com>
2022-02-20 13:10:40 +0000

    requests to aiohttp & commented out error stuff.

--- app/utlity.py
+++ app/utlity.py
@@ -1,7 +1,7 @@
 # TODO: Move this to a core.py cog.
 
+from urllib import response
 import aiohttp
-import requests
 import json
 from jsonmerge import merge
 
@@ -18,15 +18,30 @@ async def readTemplate(template):
     """
     return json.load(open(f"app/templates/{template}.json"))
 
-async def requestType(url, type, headers, body=None):
-    if type == 'post':
-        response = requests.post(url=url, json=body, headers=headers)
-    if type == 'get':
-        response = requests.get(url=url, headers=headers)
-    if type == 'put':
-        response = requests.put(url=url, json=body, headers=headers)
-    if type == 'del':
-        response = requests.delete(url=url, json=body, headers=headers)
+async def asyncRequestType(url, type, headers, body=None):
+    async with aiohttp.ClientSession(headers=headers) as session:
+        if type == 'post':
+            async with session.post(url=url, json=body) as response:
+                return response
+        if type == 'get':
+            async with session.get(url=url) as response:
+                return response
+        if type == 'put':
+            async with session.put(url=url, json=body) as response:
+                return response
+        if type == 'del':
+            async with session.delete(url=url, json=body) as response:
+                return response
+
+# async def requestType(url, type, headers, body=None):
+#     if type == 'post':
+#         response = requests.post(url=url, json=body, headers=headers)
+#     if type == 'get':
+#         response = requests.get(url=url, headers=headers)
+#     if type == 'put':
+#         response = requests.put(url=url, json=body, headers=headers)
+#     if type == 'del':
+#         response = requests.delete(url=url, json=body, headers=headers)
 
     return response
 
@@ -46,6 +61,7 @@ async def callJfaApi(endpoint, type, header, body=None):
         "Authorization": await grabJfaKey()
     }
     headers = merge(header, headers)
-
-    response = await requestType(url=url, type=type, body=body, headers=headers)
-    return response.content, response.status_code
+    # FIXME: json return causing errors when used.
+    response = await asyncRequestType(url=url, type=type, body=body, headers=headers)
+    json = await response.json()
+    return json, response.status
