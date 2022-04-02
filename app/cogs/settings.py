9d0f7348b8e6aef667f3015b401d4082a4599be5
James Finch <LubricantJam@users.noreply.github.com>
2022-04-02 19:51:59 +0100

    Removed other content types.

--- app/cogs/settings.py
+++ app/cogs/settings.py
@@ -27,7 +27,7 @@ class Settings(commands.Cog):
         if access == "shows":
             content = ["episode"]
         elif access == "livetv":
-            content = ["livetvchannel", "livetvprogram", "tvchannel", "tvprogram"]
+            content = ["tvchannel"]
         json = {"UserId": userid, "MediaTypes": content}
         headers = {
             "Authorization": "Basic " + base64.b64encode(
